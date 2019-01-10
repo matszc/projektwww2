76bdde80e03a18d1cd39e66e28f732874c33aee2
matszc <33006487+matszc@users.noreply.github.com>
2019-01-10 14:18:42 +0100

    make template

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"/>
+    <title>ProjektPHP</title>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <div class="logo">
+            <img src="img/lagrande.jpg" alt="logo">
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="#">Zestawy</a></li>
+                <li><a href="#">Pizza</a></li>
+                <li><a href="#">Bezmiesne</a></li>
+                <li><a href="#">Pasty</a></li>
+                <li><a href="#">Sosy</a></li>
+                <li><a href="#">Napoje</a></li>
+            </ul>
+        </div>
+        <div class="card">
+            <ul>
+                <li>Zapraszamy na smaczne obiady i zestawy dnia!</li>
+                <li>schabowy + zupa już za 13,00 zł</li>
+                <li>chlopskie jadlo + zupa już za 13,00 zł</li>
+                <li>paluszki z kurczaka+ zupa już za 13,00 zł</li>
+                <li>zeberka pieczone w kapuscie + zupa już za 13,00 zł</li>
+            </ul>
+        </div>
+    </div>
+    <div class="pizza">
+    </div>
+</div>
+</body>
+</html>
