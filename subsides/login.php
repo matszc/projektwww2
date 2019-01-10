93bdf4b422c6c4a46395f5687c193cfcb287ac12
matszc <33006487+matszc@users.noreply.github.com>
2019-01-10 15:47:28 +0100

    add login form

---
+++ subsides/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>logowanie</title>
+    <link type="text/css" rel="stylesheet" href="../css/style.css"/>
+</head>
+<body>
+    <div class="form">
+    <form action="login_check.php" method="post">
+        <label>Login</label>
+        <input type=text><br/><br/>
+        <label>Hasło</label>
+        <input type="password"><br/><br/>
+        <input type="submit" value="zaloguj">
+    </form>
+    </div>  
+</body>
+</html>
